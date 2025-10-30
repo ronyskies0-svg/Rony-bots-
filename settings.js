c18d2262f8324e4be47c3ae7eea79d3f9afd3926
ronyskies0-svg <ronyskies0@gmail.com>
2025-10-30 23:49:13 +0200

    Add settings.js for bot configuration

---
+++ settings.js
@@ -0,0 +1,26 @@
+const settings = {
+  packname: 'Rony⚠️SKIES',
+  author: 'Ronaldo Mazive',
+  botName: "Rony⚠️SKIES",
+  botOwner: 'Ronaldo Mazive', 
+  timezone: 'Africa/Accra',
+  prefix: '.',
+  ownerNumber: '+258871374864', 
+  AUTO_STATUS_REACT: 'true',
+  AUTO_STATUS_REPLY: 'false',
+  AUTO_STATUS_MSG: 'Status Viewed Queen Riam',
+
+  AUTORECORD: 'false',
+  AUTOTYPE: 'false',
+  AUTORECORDTYPE: 'false',
+  
+
+  
+
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
